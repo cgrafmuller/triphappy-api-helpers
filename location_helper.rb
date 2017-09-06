969d74e4849fe3c955c223e9de9f960dad7b621e
Carl Grafmuller <cgrafmuller@gmail.com>
2017-09-06 11:38:50 -0300

    Geo-locate IP addresses with Free Geo IP

---
+++ location_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Provides IP geo-location calls
+
+module LocationHelper
+  include HTTParty
+  class Location
+    def self.convert_ip_to_iso2(ip)
+      response = HTTParty.get('http://freegeoip.net/json/' + ip)
+      return response['country_code'].to_s.empty? ? 'US' : response['country_code']
+    end
+
+    def self.convert_km_to_mi(distance)
+      return distance.to_f / 1.60934
+    end
+  end
+end
